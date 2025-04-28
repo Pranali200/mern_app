8a51163d3fb4298f8aafa5e9c6cea7b09c9ff2c0
Pranali200 <hemanepranali08@gmail.com>
2025-04-29 01:15:33 +0530

    auth API structure reset

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,14 +3,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
-    const { email, password } = req.body;
-
+    const { name, email, password } = req.body;
     try {
         let user = await User.findOne({ email });
         if (user) return res.status(400).json({ message: 'User already exists' });
 
         const hashedPassword = password ? await bcrypt.hash(password, 10) : undefined;
-        user = new User({ email, password: hashedPassword });
+        user = new User({ name, email, password: hashedPassword });
         await user.save();
 
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
@@ -20,6 +19,7 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email:{ type :String, required:true, unique: true},
-    password:{type: String},
+    name: { type: String, required: true }, 
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
 });
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema);
